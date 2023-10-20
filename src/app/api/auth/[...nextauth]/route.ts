b33e5cec9858eea89bebd4d477f5791c9f97fe38
Marcello Nazar <marcellonazarb@hotmail.com>
2023-10-20 15:02:13 -0300

    atualizando

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -16,36 +16,7 @@ export const authOptions = {
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        await connect();
-        try {
-          const user = await User.findOne({ email: credentials!.email });
-          if (user) {
-            const checkPassword = await bcrypt.compare(
-              credentials!.password,
-              user.password
-            );
-            if (checkPassword) {
-              return user;
-            } else {
-              console.log("verificação da senha");
-            }
-          } else
-            (err: any) => {
-              console.log(err);
-            };
-        } catch (err: any) {
-          console.log(err);
-        }
-      },
-    }),
+    
   ],
   pages: {
     error: "/login",
